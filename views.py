a545adf7409fdb6ebc9caa4c492a9c7773aab012
Bartosz Dobrosielski <dobry1@pm.me>
2020-09-08 19:58:47 +0200

    add delete funcionality and css

--- views.py
+++ views.py
@@ -1,16 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import requests
 from .models import City
 from .forms import CityForm
 
 
 def index(request):
-    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&APPID={key}'
+    key = '9bb88af909d85a9aeff78756263783c7'
+    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&APPID=9bb88af909d85a9aeff78756263783c7'
 
+
+    errorMsg = ''
+    message_class = '' # css class
+    message = '' # msg for user
 
     if request.method == 'POST':
         form = CityForm(request.POST)
-        form.save()
+
+        if form.is_valid():
+            newCity = form.cleaned_data['name']
+            existingCity = City.objects.filter(name=newCity).count()
+
+        if(existingCity == 0): #if city dont exist
+            r = requests.get(url.format(newCity)).json()
+
+            if r['cod'] == 200:
+                form.save()
+            else:
+                errorMsg = "City dont exist at all"
+
+        else:
+            errorMsg = "city alredy added"
+
+
+
+        if errorMsg:
+            message = errorMsg
+            message_class = 'is-danger'
+        else:
+            message = 'City added successfully!!!'
+            message_class = 'is-success'
 
     form = CityForm()
 
@@ -34,8 +62,19 @@ def index(request):
         weather_data.append(city_weather)
 
 
-    context = {'weather_data' : weather_data, 'form':form}
+    context = {
+        'weather_data' : weather_data,
+        'form':form,
+        'message' : message,
+        'message_class' : message_class
+
+        }
 
     return render(request, 'weather.html', context)
+
+def deleteCity(request, cityName):
+    City.objects.get(name=cityName).delete()
+
+    return redirect('home')
 
 # Create your views here.
